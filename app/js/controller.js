13deb8a1d59eccf18e657a0ef214132e4c0eeabb
Tiago Lopes <tiago.lopes@blip.pt>
2016-02-09 22:34:23 +0000

    Add controller

---
+++ app/js/controller.js
@@ -0,0 +1,30 @@
+var app = angular.module('angularTraining', []);
+
+app.controller('AngularTrainingController', function ($scope) {
+    $scope.footballPlayers = [
+        'Lionel Messi',
+        'Cristiano Ronaldo',
+        'Xavi Midfielder',
+        'Andres Iniesta',
+        'Zlatan Ibrahimovic',
+        'Radamel Falcao',
+        'Robin van Persie',
+        'Andrea Pirlo',
+        'Yaya Toure',
+        'Edinson Cavani',
+        'Sergio Aguero',
+        'Iker Casillas',
+        'Neymar',
+        'Sergio Busquets',
+        'Xabi Alonso',
+        'Thiago Silva',
+        'Mesut Ozil',
+        'David Silva',
+        'Bastian Schweinsteiger',
+        'Gianluigi Buffon',
+        'Luis Suarez',
+        'Sergio Ramos',
+        'Gerard Pique',
+        'Philipp Lahm'
+    ];
+});
