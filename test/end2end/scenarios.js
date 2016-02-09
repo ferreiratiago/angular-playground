b7c3159c9d94c07884dbcefb54d71803a9731682
Tiago Lopes <tiago.lopes@blip.pt>
2016-02-09 22:42:24 +0000

    Update end2end and unit tests

--- test/end2end/scenarios.js
+++ test/end2end/scenarios.js
@@ -1,37 +1,23 @@
 describe('AngularTraining App', function() {
 
-    // test suite
-    describe('Football players list view', function() {
+    describe('Git repos list view', function() {
 
-        // foreach each test case get the url 'app/index.html'
         beforeEach(function() {
           browser.get('app/index.html');
         });
 
-        // test case
         it('should filter the list as the user types into the search box', function() {
-
-            // get all html elements build by the repeater
-            var playersList = element.all(by.repeater('player in footballPlayers'));
-            // get the query box
+            var repos = element.all(by.repeater('repo in gitRepos'));
             var query = element(by.model('query'));
 
-            // check if the initial page state is as expected
-            expect(playersList.count()).toBe(24);
-            // browser.pause(5001);
+            expect(repos.count()).toBeGreaterThan(10);
 
-            // simulates user typing
-            query.sendKeys('silva');
-            // check if the filter worker properly
-            expect(playersList.count()).toBe(2);
-            // browser.pause(5002);
+            query.sendKeys('dart');
+            expect(repos.count()).toBeGreaterThan(2);
 
-            // clear the query box
             query.clear();
-            query.sendKeys('ronaldo');
-            // only 1 Ronaldo! SIIIIIIIIIIIMM!
-            expect(playersList.count()).toBe(1);
-            // browser.pause(5003);
+            query.sendKeys('phonecat');
+            expect(repos.count()).toBe(1);
         });
     });
 });
--- test/unit/controller-spec.js
+++ test/unit/controller-spec.js
@@ -1,19 +1,29 @@
-// test suite
 describe('AngularTrainingController', function(){
+    var ctrl;
+    var http;
+    var scope;
 
-    // before each test load the angularTraining module.
     beforeEach(module('angularTraining'));
 
-    // test case
-    it('should create "footballPlayers" model with 24 players',
-        // inject the $controller service into our test function
-        inject(function($controller) {
-            // mock scope
-            var scope = {};
-            // use $controller to create an instance of the AngularTrainingController
-            var ctrl = $controller('AngularTrainingController', {$scope:scope});
+    beforeEach(inject(function($httpBackend, $rootScope, $controller) {
+        http = $httpBackend;
+        http.expectGET('https://api.github.com/users/angular/repos')
+            .respond([{
+                name: 'angular',
+                html_url: 'www.angular.io'
+            }]);
 
-            // verify that the footballPlayers array property on the scope contains 24 records.
-            expect(scope.footballPlayers.length).toBe(24);
+        scope = $rootScope.$new();
+        ctrl = $controller('AngularTrainingController', {$scope: scope});
     }));
+
+    it('should load the git repos from angular', function () {
+        expect(scope.gitRepos).toBeUndefined();
+        http.flush();
+
+        expect(scope.gitRepos).toEqual([{
+            name: 'angular',
+            html_url: 'www.angular.io'
+        }]);
+    });
 });
