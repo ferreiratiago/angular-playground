d3f49ca580c6bdfc489e1620e2caee6b04fb51dc
Tiago Lopes <tiago.lopes@blip.pt>
2016-02-09 22:42:05 +0000

    Add service

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,30 +1,7 @@
 var app = angular.module('angularTraining', []);
 
-app.controller('AngularTrainingController', function ($scope) {
-    $scope.footballPlayers = [
-        'Lionel Messi',
-        'Cristiano Ronaldo',
-        'Xavi Midfielder',
-        'Andres Iniesta',
-        'Zlatan Ibrahimovic',
-        'Radamel Falcao',
-        'Robin van Persie',
-        'Andrea Pirlo',
-        'Yaya Toure',
-        'Edinson Cavani',
-        'Sergio Aguero',
-        'Iker Casillas',
-        'Neymar',
-        'Sergio Busquets',
-        'Xabi Alonso',
-        'Thiago Silva',
-        'Mesut Ozil',
-        'David Silva',
-        'Bastian Schweinsteiger',
-        'Gianluigi Buffon',
-        'Luis Suarez',
-        'Sergio Ramos',
-        'Gerard Pique',
-        'Philipp Lahm'
-    ];
+app.controller('AngularTrainingController', function ($scope, $http) {
+    $http.get('https://api.github.com/users/angular/repos').success(function(data) {
+      $scope.gitRepos = data;
+    });
 });
